33edde2a34c284a3f3b06fd3464fc0bad2398a41
LAB_E502 <labe502@continental.edu.pe>
2025-04-16 11:18:04 -0500

    Se añadio las reglas de PEP8

--- FiguraGeometrica.py
+++ FiguraGeometrica.py
@@ -2,6 +2,7 @@
 
 from abc import ABC, abstractmethod
 
+
 class FiguraGeometrica(ABC):
     """Clase abstracta que define la interfaz para calcular áreas."""
     @abstractmethod
@@ -11,50 +12,50 @@ class FiguraGeometrica(ABC):
 
 
 class Rectangulo(FiguraGeometrica):
-    def __init__(self, base , altura):
-        self.base=base
-        self.altura=altura
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
 
     def calcular_area(self):
         """Calcula el área del rectángulo."""
-        area=self.base*self.altura
+        area = self.base*self.altura
         return area
 
 
 class Triangulo(FiguraGeometrica):
-    def __init__(self, base , altura):
-        self.base=base
-        self.altura=altura
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
 
     def calcular_area(self):
         """Calcula el área del triángulo."""
-        area=(self.base*self.altura)/2
+        area = (self.base*self.altura)/2
         return area
 
 
 class Circulo(FiguraGeometrica):
     def __init__(self, radio):
-        self.pi =3.14159
-        self.radio=radio
+        self.pi = 3.14159
+        self.radio = radio
 
     def calcular_area(self):
         """Calcula el área del círculo."""
-        area=self.pi*(self.radio**2)
+        area = self.pi*(self.radio**2)
         return area
 
 
 # Variables globales
-BASE_RECTANGULO =10
-ALTURA_RECTANGULO=5
-BASE_TRIANGULO=7
-ALTURA_TRIANGULO =4
-RADIO_CIRCULO =3
+BASE_RECTANGULO = 10
+ALTURA_RECTANGULO = 5
+BASE_TRIANGULO = 7
+ALTURA_TRIANGULO = 4
+RADIO_CIRCULO = 3
 
 # Ejecución principal
 if __name__ == "__main__":
     # Crear instancias de las figuras
-    rectangulo = Rectangulo(BASE_RECTANGULO,ALTURA_RECTANGULO)
-    triangulo = Triangulo(BASE_TRIANGULO,ALTURA_TRIANGULO)
+    rectangulo = Rectangulo(BASE_RECTANGULO, ALTURA_RECTANGULO)
+    triangulo = Triangulo(BASE_TRIANGULO, ALTURA_TRIANGULO)
     circulo = Circulo(RADIO_CIRCULO)
 
     # Calcular áreas
